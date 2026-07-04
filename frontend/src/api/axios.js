bb5950438ff3f4b3856483529670d527fb8f2b4d
Abhiman5ingh <2k23.csaiml2311553@gmail.com>
2026-07-04 06:19:58 +0530

    Update API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://lastmile-delivery-tracker-3vcn.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
